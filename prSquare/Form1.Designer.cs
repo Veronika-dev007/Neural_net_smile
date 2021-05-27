669944bcb60a2c8cfe97b7e9cc962a0295ab35f1
Veronika-dev007 <loleganger@gmail.com>
2021-05-27 16:45:50 +0300

    First commit

---
+++ prSquare/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace prSquare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStudy = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.btnTrFun = new System.Windows.Forms.Button();
+            this.btnTrSad = new System.Windows.Forms.Button();
+            this.picBig = new System.Windows.Forms.PictureBox();
+            this.picDraw = new System.Windows.Forms.PictureBox();
+            this.cmb = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBig)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDraw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStudy
+            // 
+            this.btnStudy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStudy.Location = new System.Drawing.Point(454, 46);
+            this.btnStudy.Name = "btnStudy";
+            this.btnStudy.Size = new System.Drawing.Size(108, 45);
+            this.btnStudy.TabIndex = 0;
+            this.btnStudy.Text = "Study";
+            this.btnStudy.UseVisualStyleBackColor = true;
+            this.btnStudy.Click += new System.EventHandler(this.btnStudy_Click);
+            // 
+            // btnResult
+            // 
+            this.btnResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnResult.Location = new System.Drawing.Point(454, 97);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(108, 43);
+            this.btnResult.TabIndex = 1;
+            this.btnResult.Text = "Result";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(604, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(590, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 39);
+            this.label2.TabIndex = 5;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnClean
+            // 
+            this.btnClean.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClean.Location = new System.Drawing.Point(454, 149);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(108, 40);
+            this.btnClean.TabIndex = 6;
+            this.btnClean.Text = "Clean";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // btnTrFun
+            // 
+            this.btnTrFun.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnTrFun.Location = new System.Drawing.Point(584, 148);
+            this.btnTrFun.Name = "btnTrFun";
+            this.btnTrFun.Size = new System.Drawing.Size(121, 41);
+            this.btnTrFun.TabIndex = 8;
+            this.btnTrFun.Text = "Train Fun";
+            this.btnTrFun.UseVisualStyleBackColor = true;
+            this.btnTrFun.Click += new System.EventHandler(this.btnTrFun_Click);
+            // 
+            // btnTrSad
+            // 
+            this.btnTrSad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnTrSad.Location = new System.Drawing.Point(721, 149);
+            this.btnTrSad.Name = "btnTrSad";
+            this.btnTrSad.Size = new System.Drawing.Size(116, 41);
+            this.btnTrSad.TabIndex = 9;
+            this.btnTrSad.Text = "Train Sad";
+            this.btnTrSad.UseVisualStyleBackColor = true;
+            this.btnTrSad.Click += new System.EventHandler(this.btnTrSad_Click);
+            // 
+            // picBig
+            // 
+            this.picBig.BackColor = System.Drawing.Color.White;
+            this.picBig.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBig.Location = new System.Drawing.Point(420, 250);
+            this.picBig.Name = "picBig";
+            this.picBig.Size = new System.Drawing.Size(200, 200);
+            this.picBig.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBig.TabIndex = 3;
+            this.picBig.TabStop = false;
+            // 
+            // picDraw
+            // 
+            this.picDraw.BackColor = System.Drawing.Color.White;
+            this.picDraw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picDraw.Location = new System.Drawing.Point(31, 25);
+            this.picDraw.Name = "picDraw";
+            this.picDraw.Size = new System.Drawing.Size(350, 350);
+            this.picDraw.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picDraw.TabIndex = 2;
+            this.picDraw.TabStop = false;
+            this.picDraw.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picDraw_MouseDown);
+            this.picDraw.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picDraw_MouseMove);
+            this.picDraw.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picDraw_MouseUp);
+            // 
+            // cmb
+            // 
+            this.cmb.FormattingEnabled = true;
+            this.cmb.Items.AddRange(new object[] {
+            "pic1",
+            "pic2",
+            "pic3",
+            "pic4",
+            "pic5",
+            "pic6"});
+            this.cmb.Location = new System.Drawing.Point(689, 250);
+            this.cmb.Name = "cmb";
+            this.cmb.Size = new System.Drawing.Size(138, 28);
+            this.cmb.TabIndex = 10;
+            this.cmb.TextChanged += new System.EventHandler(this.cmb_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 475);
+            this.Controls.Add(this.cmb);
+            this.Controls.Add(this.btnTrSad);
+            this.Controls.Add(this.btnTrFun);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picBig);
+            this.Controls.Add(this.picDraw);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.btnStudy);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Smile";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBig)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDraw)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStudy;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.PictureBox picDraw;
+        private System.Windows.Forms.PictureBox picBig;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Button btnTrFun;
+        private System.Windows.Forms.Button btnTrSad;
+        private System.Windows.Forms.ComboBox cmb;
+    }
+}
+
---
+++ prSquare/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+using System.IO;
+
+using FANNCSharp;
+#if FANN_FIXED
+using FANNCSharp.Fixed;
+using DataType = System.Int32;
+#elif FANN_DOUBLE
+using FANNCSharp.Double;
+using DataType = System.Double;
+#else
+using FANNCSharp.Float;
+using DataType = System.Single;
+#endif
+
+namespace prSquare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NeuralNet net;
+        float[] input;
+
+        Pen p;
+        bool isMouseDown;
+        Point lastPoint;
+        List<string> lstPic1 = new List<string>();
+        Image resImg;
+        Bitmap pic,bmp;
+        StringBuilder sb;
+         
+        int k=10;
+        int[] arr;
+
+        private Image CompressImage(Image img)
+        {
+            Bitmap res = new Bitmap(10, 10);
+            using (Graphics gr = Graphics.FromImage(res))
+            {
+                gr.DrawImage(img, 0, 0, res.Width, res.Height);
+            }
+            return res;
+        }
+
+        public void BasePic(Bitmap sourceBmp)
+        {
+            lstPic1.Clear();
+            resImg = CompressImage(Image.FromHbitmap(sourceBmp.GetHbitmap()));
+            picBig.Image = resImg;
+            bmp = new Bitmap(resImg);
+           
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color col = bmp.GetPixel(x, y);
+                    if (col.R >= 150 && col.R <= 255 && col.G >= 150 && col.G <= 255 && col.B >= 150 && col.B <= 255)
+                    {
+                        lstPic1.Add(0.ToString());
+                    }
+                    else
+                    {
+                        lstPic1.Add(1.ToString());
+                    }
+                }
+            }
+        }
+        public void outputToNote(List<string> lst,string a)
+        {
+            using (StreamWriter sw = new StreamWriter("test.data", true))
+            {
+                string str = "";
+                foreach (string s in lst)
+                {
+                    str += s + " ";
+                }
+                sw.WriteLine(str+"\n"+a);
+            }
+        }
+        private void train(int[] m)
+        {
+            using (TrainingData data = new TrainingData())
+            {
+                if (File.Exists("test_float.net"))
+                {
+                    label1.Text = "connect yes";
+                    net = new NeuralNet("test_float.net");
+                    input = new float[100];
+                    for (int i = 0; i < 100; i++)
+                        input[i] = m[i];
+                    float[] calc_out = net.Run(input);
+                    label1.Text += "\n sad=" + calc_out[0].ToString();
+                    label1.Text += "\n smile=" + calc_out[1].ToString();
+                    if (calc_out[0] > calc_out[1]) label2.Text = "Грустный смайлик";
+                    else label2.Text = "Весёлый смайлик";
+                }
+            }
+        }
+        private void btnStudy_Click(object sender, EventArgs e)
+        {
+            uint num_input = 100;
+            uint num_out = 2;
+            uint num_hidden = 101;
+            uint num_layar = 3;
+            float num_error = 0.001f;
+            uint max_epochs = 50000;
+            uint epochs_between = 1000;
+
+            net = new NeuralNet(NetworkType.LAYER, num_layar, num_input, num_hidden, num_out);
+            using (net)
+            {
+                net.ActivationFunctionHidden = ActivationFunction.SIGMOID_SYMMETRIC;
+                net.ActivationFunctionOutput = ActivationFunction.SIGMOID_SYMMETRIC;
+
+                net.TrainOnFile("test.data", max_epochs, epochs_between, num_error);
+                net.Save("test_float.net");
+            }
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            BasePic((Bitmap)picDraw.Image);
+            arr = new int[100];
+            for (int i = 0; i < lstPic1.Count(); i++)
+            {
+                arr[i] = Convert.ToInt32(lstPic1[i]);
+            }
+            train(arr);
+        }
+        
+        private void btnTrFun_Click(object sender, EventArgs e)
+        {
+            replaceLine();
+            outputToNote(lstPic1, "0 1");
+        }
+        private void btnTrSad_Click(object sender, EventArgs e)
+        {
+            replaceLine();
+            outputToNote(lstPic1, "1 0");
+        }
+        public void replaceLine() 
+        {
+            string[] lines = File.ReadAllLines("test.data");
+            k++;
+                sb = new StringBuilder(lines[0]);
+                sb.Remove(0, 2);
+                sb.Insert(0, k);
+                lines[0] = sb.ToString();
+
+            File.WriteAllText("test.data", string.Empty);
+            File.WriteAllLines("test.data",lines);
+
+            sb =null;
+        }
+        private void picDraw_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+            lastPoint = Point.Empty;
+        }
+
+        private void picDraw_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = e.Location;
+            isMouseDown = true;
+        }
+
+        private void picDraw_MouseMove(object sender, MouseEventArgs e)
+        {
+            p = new Pen(Color.Black, 30f);
+            p.EndCap = LineCap.Round;
+            p.StartCap = LineCap.Round;
+
+            if (isMouseDown == true)
+            {
+                if (lastPoint != null)
+                {
+                    if (picDraw.Image == null)
+                    {
+                        pic = new Bitmap(picDraw.Width, picDraw.Height);
+                        picDraw.Image = pic;
+                    }
+
+                    using (Graphics g = Graphics.FromImage(pic))
+                    {
+                        g.DrawLine(p, lastPoint, e.Location);
+                    }
+
+                    picDraw.Invalidate();
+                    lastPoint = e.Location;
+
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] lin = File.ReadAllLines("test2.data");
+            File.WriteAllText("test.data", string.Empty);
+            File.WriteAllLines("test.data", lin);
+        }
+
+        private void cmb_TextChanged(object sender, EventArgs e)
+        {
+            clean_pic();
+            picDraw.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject(cmb.Text);
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            clean_pic();
+        }
+        public void clean_pic() 
+        {
+            if (picDraw.Image != null)
+            {
+                picDraw.Image = null;
+                Invalidate();
+            }
+            input = null;
+            lstPic1.Clear();
+            arr = null;
+            resImg = null;
+            label2.Text = "";
+            
+        }
+    }
+}
